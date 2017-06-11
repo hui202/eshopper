015a3c5535aed42d0690a5e3c185bb42dc66a55e
Andrew <hui_andrew@hotmail.com>
2017-06-11 04:17:34 -0700

    eloquent rom

--- larashop/routes/web.php
+++ larashop/routes/web.php
@@ -40,3 +40,39 @@ Route::get('blade', function(){
 	$drinks = array('Vodka', 'Gin', 'Brandy');
 	return view('page', array('name'=>'The Raven', 'day'=>'Friday', 'drinks'=>$drinks));
 });
+
+Route::get('/insert', function(){
+	App\Category::create(array('name' => 'Music'));
+	return 'category added';
+});
+
+Route::get('/read', function(){
+	$category = new App\Category();
+	$data = $category->all(array('name', 'id'));
+	foreach($data as $list){
+		echo $list->id . ' ' . $list->name . '';
+	}
+});
+
+Route::get('/update', function(){
+	$category = App\Category::find(6);
+	$category->name = 'HEAVY METAL';
+	$category->save();
+
+	$data = $category->all(array('name','id'));
+
+	foreach($data as $list){
+		echo $list->id . ' ' . $list->name . '';
+	}
+});
+
+Route::get('/delete', function(){
+	$category = App\Category::find(5);
+	$category->delete();
+
+	$data = $category->all(array('name', 'id'));
+
+	foreach($data as $list){
+		echo $list->id . ' ' . $list->name . '';
+	}
+});
