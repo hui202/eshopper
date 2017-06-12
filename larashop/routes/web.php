1063693ab8971a4ed3b1d35c43eb899051a7c4fc
Andrew <hui_andrew@hotmail.com>
2017-06-11 20:40:49 -0700

    RESTful API

--- larashop/routes/web.php
+++ larashop/routes/web.php
@@ -92,3 +92,28 @@ Route::get('/checkout', [
 	'middleware' => 'auth',
 	'uses' => 'Front@checkout'
 	]);
+
+//RESTful API
+Route::get('/api/v1/products/{id?}', ['middleware' => 'auth.basic', function($id = null) {
+	if ($id == null){
+		$products = App\Product::all(array('id', 'name', 'price'));
+	}
+	else {
+		$products = App\Product::find($id, array('id', 'name', 'price'));
+	}
+}]);
+
+Route::get('/api/v1/categories/{id?}', ['middleware' => 'auth.basic', function($id = null){
+	if ($id == null){
+		$categories = App\Category::all(array('id', 'name'));
+	}
+	else {
+		$categories = App\Category::find($id, array('id', 'name'));
+	}
+
+	return Response::json(array(
+		'error' => false,
+		'user' => $categories,
+		'status_code' => 200
+	));
+}]);
